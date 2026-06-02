1af79a7d3b55707ced09c9c98564e45eb82fdebe
Aman kumar vishwakrma <186990161+amankv1234@users.noreply.github.com>
2026-06-02 12:09:02 +0530

    learning

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import type {NextPage} from 'next'
+
+const DashboardPage: NextPage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>
+                I am Aman
+            </h1>
+        </div>
+    )
+}
+
+export default DashboardPage;
---
+++ app/dashboard/profile/page.tsx
@@ -0,0 +1,13 @@
+import type {NextPage} from 'next'
+
+const DashboardPage: NextPage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>
+                This is profile page
+            </h1>
+        </div>
+    )
+}
+
+export default DashboardPage;
